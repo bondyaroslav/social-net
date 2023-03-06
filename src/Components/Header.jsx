c3a8ece89236a8f5f8e1c808111e9d4c68359f33
bondyaroslav <bondy2526@gmail.com>
2023-03-06 20:30:03 +0200

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className={"Header"}>
+            <div className="logo">Company Name</div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Main/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className={"Sidebar"}>
+            <ul>
+                <li>Profile</li>
+                <li>Messages</li>
+                <li>News</li>
+                <li>Music</li>
+                <li>Settings</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
