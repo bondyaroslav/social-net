75d216d68e38389edce3704fafc22dfd1de9a1d5
bondyaroslav <bondy2526@gmail.com>
2023-10-19 16:18:35 +0300

    add pagination in Users

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,53 +1,59 @@
 import React from "react";
-import axios from "axios";
-import styles from "./Users.module.css"
+import styles from "./Users.module.css";
 
 class Users extends React.Component {
+    constructor(props) {
+        super(props);
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                response.data.items.map(item => {
-                    if (this.props.users.length === 0) {
-                        this.props.setUsers(item)
-                    }
-                })
-            })
+        this.state = {
+            users: [],
+        };
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
+    componentDidMount() {
+        this.fetchUsers(this.props.currentPage, this.props.pageSize);
+    }
+
+    fetchUsers(page, pageSize) {
+        const url = `https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${pageSize}`
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ users: data.items })
             })
     }
 
     render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+        const pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
 
-        const pages = []
+        const pages = [];
         for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
+            pages.push(i);
         }
+
         return (
             <div className={styles.Users}>
-                <div>
-                    {pages.map(page => {
-                            return <span className={this.props.currentPage === page && styles.selectedPage}
-                                         onClick={ (event) => {this.onPageChanged(page)} }>{page}
-                                    </span>
-                        }
-                    )}
+                <div className={styles.pages}>
+                    {pages.map((page) => (
+                        <span
+                            className={styles.page}
+                            key={page}
+                            onClick={() => this.fetchUsers(page, this.props.pageSize)}
+                        >
+                            {page}
+                        </span>
+                    ))}
                 </div>
-                {this.props.users.map(user =>
+                {this.state.users.map((user) => (
                     <div className={styles.user} key={user.id}>
                         <p>{user.id}</p>
                         <p>{user.name}</p>
-                    </div>)}
+                    </div>
+                ))}
             </div>
-        )
+        );
     }
 }
 
-export default Users
+export default Users;
