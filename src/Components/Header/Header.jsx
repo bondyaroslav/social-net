ea883b381b6b0652cfac1c51501cb92ed17054a1
bondyaroslav <bondy2526@gmail.com>
2023-12-19 23:51:30 +0200

    feat: add logo in Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from 'react'
+import styles from './Header.module.css'
+import companyLogo from "../../assets/images/companyLogo.png"
 
 const Header = () => {
     return (
         <div className={styles.Header}>
-            <div className={styles.logo}>Company Name</div>
+            <img className={styles.logo} src={companyLogo} alt="asd"/>
         </div>
-    );
-};
+    )
+}
 
-export default Header;
+export default Header
