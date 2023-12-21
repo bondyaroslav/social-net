10098c4ca55988a55e5cebc00e5d2a494167ef7b
bondyaroslav <bondy2526@gmail.com>
2023-12-21 11:59:53 +0200

    feat: DAL

---
+++ src/api/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/"
+})
+
+export const usersAPI = {
+    getUsers(page, pageSize) {
+        instance.get(`users?page=${page}&count=${pageSize}`)
+            .then( response => {
+                return response.data
+            })
+    },
+    follow(userId) {
+        instance.post(`follow/${userId}`)
+            .then( response => response.data )
+    },
+    unfollow(userId) {
+        instance.post(`follow/${userId}`)
+            .then( response => response.data )
+    }
+}
