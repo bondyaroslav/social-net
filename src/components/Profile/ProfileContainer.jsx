7e86fa70297c47f79247e142e45504e44ba1da20
bondyaroslav <bondy2526@gmail.com>
2024-02-29 22:08:05 +0200

    feat(Profile): follow/unfollow

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,20 +1,20 @@
 import React, {useEffect} from "react"
 import Profile from "./Profile"
 import { useDispatch, useSelector } from "react-redux"
-import { setUserProfile } from "../../store/reducers/profileReducer"
+import { setUserProfileAC } from "../../store/reducers/profileReducer"
 import {useParams} from "react-router-dom"
 import axios from "axios"
 
 const ProfileContainer = (props) => {
     const dispatch = useDispatch()
-    const profile = useSelector((profile) => profile.profilePage.profile)
+    const profile = useSelector(state => state.profilePage.profile)
     const authUserId = useSelector(state => state.auth.id)
     let {userId} = useParams()
 
     useEffect(() => {
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${props.userId || userId}`)
             .then((response) => {
-                dispatch(setUserProfile(response.data))
+                dispatch(setUserProfileAC(response.data))
             })
     }, [userId])
 
--- src/components/Profile/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo.jsx
@@ -1,10 +1,27 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {Box} from "@mui/system"
 import userPhoto from "../../assets/images/userPhoto.jpg"
 import {Button, Typography} from "@mui/material"
 import {NavLink} from "react-router-dom"
+import {follow, unfollow} from "../../api/usersApi"
+import axios from "axios"
+import {useDispatch} from "react-redux"
 
 const ProfileInfo = ({profile, isItAuthUserAccount}) => {
+
+    const dispatch = useDispatch()
+    const [isUserFollow, setIsUserFollow] = useState(null)
+
+    const getIsUserFollow = () => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/follow/${profile.userId}`, {withCredentials: true})
+            .then((response) => {
+                setIsUserFollow(response.data)
+            })
+    }
+
+    useEffect(() => {
+        getIsUserFollow()
+    }, [follow(), unfollow()])
 
     return (
         <Box>
@@ -14,12 +31,10 @@ const ProfileInfo = ({profile, isItAuthUserAccount}) => {
                     : <Box style={{width: "100%", height: 150, backgroundColor: "whitesmoke"}}></Box>
                 }
             </Box>
-
-            <Box style={{display: "flex", flexDirection: "row", margin: 20}}>
-                {profile.photos.small ?
-                    <img src={profile.photos.small}/>
-                    :
-                    <img src={userPhoto} style={{width: "10%", height: "10%"}}/>
+            <Box style={{display: "flex", flexDirection: "row"}}>
+                {profile.photos.small
+                    ? <img src={profile.photos.small}/>
+                    : <img src={userPhoto} style={{width: "10%", height: "10%"}}/>
                 }
                 <Box style={{marginLeft: 20}}>
                     <Typography style={{fontSize: 30}}>{profile.fullName}</Typography>
@@ -31,12 +46,22 @@ const ProfileInfo = ({profile, isItAuthUserAccount}) => {
                     </Box>
                     {isItAuthUserAccount
                         ? null
-                        : <NavLink to={"/dialogs"}><Button>send message</Button></NavLink>
+                        : <NavLink to={`/messages/${profile.userId}`}><Button>send message</Button></NavLink>
+                    }
+                </Box>
+                <Box style={{display: "flex", alignItems: "center", marginLeft: 50}}>
+                    {isItAuthUserAccount
+                        ? null
+                        : <>
+                            {isUserFollow
+                                ? <Button onClick={() => dispatch(unfollow(profile.userId))} style={{width: 120, height: 50, fontSize: 18}}>unfollow</Button>
+                                : <Button onClick={() => dispatch(follow(profile.userId))} style={{width: 120, height: 50, fontSize: 18}}>follow</Button>
+                            }
+                        </>
                     }
                 </Box>
             </Box>
         </Box>
     )
 }
-
 export default ProfileInfo
