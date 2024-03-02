9fd39f84b0c630450892be2abac3d8c09c87841f
bondyaroslav <bondy2526@gmail.com>
2024-03-02 15:20:31 +0200

    feat(Users): add send message button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react"
 import userPhoto from "../../assets/images/userPhoto.jpg"
-import {NavLink} from "react-router-dom"
+import {Link, NavLink} from "react-router-dom"
 import Preloader from "../Preloader"
 import {Box, Container} from "@mui/system"
 import {Button, Card, Pagination, Typography} from "@mui/material"
@@ -34,18 +34,25 @@ const Users = (
                 :
                 <Box>
                     {users.map((user) => (
-                        <Card key={user.id} style={{display: "flex", alignItems: "flex-start", margin: 10}}>
-                            <Box style={{maxWidth: 120, height: 120}}>
-                                <NavLink to={`/profile/${user.id}`} style={{display: "flex", flexDirection: "column"}}>
+                        <Card key={user.id} style={{display: "flex", justifyContent: "space-between", marginTop: 10}}>
+                            <Box style={{display: "flex"}}>
+                                <NavLink to={`/profile/${user.id}`} style={{display: "flex", flexDirection: "column", maxWidth: 120, height: 120}}>
                                     <img src={user.photos.small || userPhoto} style={{width: 120}}/>
                                 </NavLink>
+                                <Box style={{display: "flex", flexDirection: "column"}}>
+                                    <Typography>{user.name}</Typography>
+                                    <Box>
+                                        {user.status
+                                            ? <Typography>{user.status}</Typography>
+                                            : <Typography>no status</Typography>
+                                        }
+                                    </Box>
+                                    <NavLink to={`/messages/${user.id}`} style={{display: "flex", flexDirection: "column", textDecoration: "none"}}>
+                                        <Button style={{width: 150, marginTop: 15}}>send message</Button>
+                                    </NavLink>
+                                </Box>
                             </Box>
-                            <Box>
-                                <Typography style={{margin: 10}}>{user.name}</Typography>
-                                {user.status
-                                    ? <Typography style={{margin: 10}}>{user.status}</Typography>
-                                    : <Typography style={{margin: 10}}>no status</Typography>
-                                }
+                            <Box style={{display: "flex", justifyContent: "center", alignItems: "center", alignContent: "center", width: "20%", height: 120}}>
                                 {user.followed
                                     ? <Button onClick={() => unfollow(user.id)}>unfollow</Button>
                                     : <Button onClick={() => follow(user.id)}>follow</Button>
