c23753caccc9564843f26ebc589ee36297a45d5f
bondyaroslav <bondy2526@gmail.com>
2024-04-26 21:11:59 +0300

    refactor db

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 
-
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
@@ -10,35 +9,6 @@ const pool = new Pool({
     password: "postgres",
     port: 5432,
 });
-
-async function sendMessage(message) {
-    const { messageId, messageSenderId, messageTakerId, messageText, messageData } = message;
-    console.log(message)
-    try {
-        const client = await pool.connect();
-        const queryText = "INSERT INTO Messages (MessageId, MessageSenderId, MessageTakerId, MessageText, MessageDate) VALUES ($1, $2, $3, $4, $5)";
-        const values = [messageId, messageSenderId, messageTakerId, messageText, messageData];
-        await client.query(queryText, values);
-        client.release();
-        return { success: true, message: "Message sent successfully" };
-    } catch (error) {
-        console.error("Error sending message:", error);
-        return { success: false, message: "An error occurred while sending the message" };
-    }
-}
-
-async function getAllMessages() {
-    try {
-        const client = await pool.connect();
-        const queryText = "SELECT * FROM Messages";
-        const result = await client.query(queryText);
-        client.release();
-        return { success: true, messages: result.rows };
-    } catch (error) {
-        console.error("Error retrieving messages:", error);
-        return { success: false, message: "An error occurred while retrieving messages" };
-    }
-}
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -56,21 +26,73 @@ app.options("*", (req, res) => {
     res.sendStatus(200);
 });
 
-app.post("/api/messages", async (req, res) => {
-    const message = req.body;
+async function getMessagesByChat(chatId) {
     try {
-        const result = await sendMessage(message);
-        res.json(result);
+        const client = await pool.connect();
+        const queryText = "SELECT * FROM Messages WHERE MessageTakerId = $1";
+        const result = await client.query(queryText, [chatId]);
+        client.release();
+        return { success: true, messages: result.rows };
+    } catch (error) {
+        console.error("Error retrieving messages by chat:", error);
+        return { success: false, message: "An error occurred while retrieving messages by chat" };
+    }
+}
+
+async function createChat(title) {
+    try {
+        const client = await pool.connect();
+        const queryText = "INSERT INTO Chats (Title) VALUES ($1) RETURNING ChatId";
+        const values = [title];
+        const result = await client.query(queryText, values);
+        client.release();
+        return { success: true, chatId: result.rows[0].ChatId };
+    } catch (error) {
+        console.error("Error creating chat:", error);
+        return { success: false, message: "An error occurred while creating the chat" };
+    }
+}
+
+async function getChat(chatId) {
+    try {
+        const client = await pool.connect();
+        const queryText = "SELECT * FROM Chats WHERE ChatId = $1";
+        const result = await client.query(queryText, [chatId]);
+        client.release();
+        return { success: true, chat: result.rows[0] };
+    } catch (error) {
+        console.error("Error retrieving chat:", error);
+        return { success: false, message: "An error occurred while retrieving chat" };
+    }
+}
+
+app.post("/api/chats/:chatId/messages", async (req, res) => {
+    const { chatId } = req.params;
+    const newMessage = req.body;
+    try {
+        newMessage.chatId = chatId;
+        res.json({ success: true, message: "Message sent successfully" });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, message: "Internal server error" });
     }
 });
 
-
-app.get("/api/messages", async (req, res) => {
+app.get("/api/chats/:chatId/messages", async (req, res) => {
+    const { chatId } = req.params
     try {
-        const result = await getAllMessages();
+        const result = await getMessagesByChat(chatId);
+        res.json(result)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ success: false, message: "Internal server error" })
+    }
+})
+
+app.get("/api/chats/:chatId", async (req, res) => {
+    const { chatId } = req.params;
+    try {
+        const result = await getChat(chatId);
         res.json(result);
     } catch (error) {
         console.error(error);
